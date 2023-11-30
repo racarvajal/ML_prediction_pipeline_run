e798e388582ff6661c2adb09f83fbe26196ecf32
racarvajal <rcarvajalp@outlook.com>
2023-11-30 13:58:27 +0000

    add file names

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ML_redshift_prediction_HETDEX
-Companion code for ML redshift prediction in HETDEX Spring and Stripe 82 Fields
+
+Jupyter notebook with example script to load and run prediction pipeline from Carvajal et al., 2023.
+
+Data and model files can be obtained from zenodo...
+
+*`running_pred_pipeline.ipynb` contains a simple code to download the files and run the prediction pipeline on them.
+*`global_variables.py` contains names and paths of files for running the code.
+*`global_functions.py` contains a few functions to run each step of the prediction pipeline.
--- global_variables.py
+++ global_variables.py
@@ -10,15 +10,14 @@ file_HETDEX         = 'predicted_rAGN_HETDEX.parquet'  # 15136878 objects (1.5e7
 file_S82            = 'predicted_rAGN_S82.parquet'     # 3590306 objects (3.6e6)
 
 # Uncalibrated models
-AGN_gal_model      = 'classification_AGN_galaxy_dec_18_2022'
-radio_model        = 'classification_LOFAR_detect_dec_19_2022'
-full_z_model       = 'regression_z_dec_20_2022'
-high_z_model       = 'regression_high_z_dec_121_2022'
+AGN_gal_model      = 'classification_AGN_galaxy'
+radio_model        = 'classification_radio_detection'
+full_z_model       = 'regression_redshift'
 
 # Calibrated models
 cal_AGN_gal_model  = 'cal_' + AGN_gal_model + '.joblib'
 cal_radio_model    = 'cal_' + radio_model   + '.joblib'
 
 # Classification calibrated thresholds
-cal_AGN_thresh     = 0.34895396724527294
-cal_radio_thresh   = 0.2046047064139296
+cal_AGN_thresh     = 0.34895 # 0.34895396724527294
+cal_radio_thresh   = 0.20460 # 0.2046047064139296
